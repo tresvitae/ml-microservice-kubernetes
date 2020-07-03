8820c1fdb730aeb947b81a2580ce8e307b9ce7fa
Patryk Futa <patryk.futa@gmail.com>
2020-07-03 22:19:09 +0200

    setup K8n

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=tresvitae/ml-microservice
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-microservices\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=ml-microservices
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-microservices 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,13 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=tresvitae/ml-microservice
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag ml-microservices $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
